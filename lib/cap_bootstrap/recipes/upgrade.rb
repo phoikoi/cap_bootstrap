410226c9166411e0e1e34649a76b5ca2bacfbd2a
Paavo Leinonen <paavo@leinonen.net>
2013-08-21 15:33:30 +0200

    Update upgrade.rb

--- lib/cap_bootstrap/recipes/upgrade.rb
+++ lib/cap_bootstrap/recipes/upgrade.rb
@@ -2,9 +2,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :upgrade do
     desc "Upgrade system"
     task :all, roles: :web do
-      run "#{sudo} export DEBIAN_FRONTEND=noninteractive"
       run "#{sudo} apt-get update -qq --force-yes"
-      run "#{sudo} apt-get upgrade -qq --force-yes"
+      run "#{sudo} apt-get upgrade -qq --force-yes", :env => { 'DEBIAN_FRONTEND' => 'noninteractive' }
     end
     after "deploy:install", "upgrade:all"
   end
