6fc9ad28e9739d16b159a639619293d23d190609
Paavo Leinonen <paavo@leinonen.net>
2013-08-21 19:21:44 +0200

    Update nginx.rb

--- lib/cap_bootstrap/recipes/nginx.rb
+++ lib/cap_bootstrap/recipes/nginx.rb
@@ -12,7 +12,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :ssl, roles: :web do
       upload "#{certificate_location}", "/tmp/server.crt"
       upload "#{certificate_key_location}", "/tmp/server.key"
-      run "#{sudo} mkdir /etc/nginx/ssl"
+      run "#{sudo} mkdir -p /etc/nginx/ssl"
       run "#{sudo} mv /tmp/server.crt /etc/nginx/ssl/server.crt"
       run "#{sudo} mv /tmp/server.key /etc/nginx/ssl/server.key"
     end
