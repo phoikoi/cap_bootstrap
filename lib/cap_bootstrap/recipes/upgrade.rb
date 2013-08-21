b315431572cc926cdcbd38fd2cdfa2c3c7e92a39
Paavo Leinonen <paavo@leinonen.net>
2013-08-21 15:25:40 +0200

    Update upgrade.rb

--- lib/cap_bootstrap/recipes/upgrade.rb
+++ lib/cap_bootstrap/recipes/upgrade.rb
@@ -1,8 +1,8 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :upgrade do
-    desc "Install latest stable release of nginx"
+    desc "Upgrade system"
     task :all, roles: :web do
-      run "export DEBIAN_FRONTEND=noninteractive"
+      run "#{sudo} export DEBIAN_FRONTEND=noninteractive"
       run "#{sudo} apt-get update -qq --force-yes"
       run "#{sudo} apt-get upgrade -qq --force-yes"
     end
