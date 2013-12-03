ac48759b6cdb69d6ead6808596ddedd96d68ec09
Peter Hull <design.services@oikoi.com>
2013-12-02 21:01:54 -0800

    Changed "create user" to "create role"

--- lib/cap_bootstrap/recipes/postgresql.rb
+++ lib/cap_bootstrap/recipes/postgresql.rb
@@ -14,7 +14,7 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc "Create a database for this application."
     task :create_database, roles: :db, only: {primary: true} do
-      run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
+      run %Q{#{sudo} -u postgres psql -c "create role #{postgresql_user} login password '#{postgresql_password}';"}
       run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
     end
     after "deploy:setup", "postgresql:create_database"
